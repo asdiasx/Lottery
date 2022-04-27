48f112bbad375e83c1b40649eb8da1f2b94f49cc
Adriano Dias <adriano.rj.br@gmail.com>
2022-04-27 12:40:09 +0000

    refactored routes and paths to work after deployment

--- index.js
+++ index.js
@@ -12,52 +12,52 @@ app.use(express.static(myDirName + '/public'));
 app.set('view engine', 'ejs');
 
 // route for ejs table
-app.get('/', async function (req, res) {
-  initialDbSync();
+app.get('/lotomania', async function (req, res) {
+await initialDbSync();
 
-  // select last n drawings from db
-  const drawings = await selectDraws(25);
+// select last n drawings from db
+const drawings = await selectDraws(25);
 
-  res.render('table.ejs', { drawings });
-  res.end();
+res.render('table.ejs', { drawings });
+res.end();
 });
 
 // route to generate pdf
 app.get('/download', async function (req, res) {
-  const browser = await puppeteer.launch({ headless: true });
-  const page = await browser.newPage();
+const browser = await puppeteer.launch({ headless: true });
+const page = await browser.newPage();
 
-  // update url to the page you want to render in pdf
-  await page.goto('http://localhost:8080', {
-    waitUntil: 'networkidle0',
-  });
+// update url to the page you want to render in pdf
+ await page.goto('https://seek4k.com.br/lotomania/', {
+   waitUntil: 'networkidle0',
+ });
 
-  await page.addStyleTag({ content: '#dldBtn {display: none;}' });
+ await page.addStyleTag({ content: '#dldBtn {display: none;}' });
 
-  await page.pdf({
-    path: './public/table.pdf',
-    scale: 0.63,
-    displayHeaderFooter: true,
-    landscape: true,
-    format: 'A4',
-    printBackground: true,
-  });
-  await browser.close();
+ await page.pdf({
+   path: './public/table.pdf',
+   scale: 0.63,
+   displayHeaderFooter: true,
+   landscape: true,
+   format: 'A4',
+   printBackground: true,
+ });
+ await browser.close();
 
-  const date = new Date().toLocaleDateString();
-  const dateFormater = (date) => {
-    date = date.split('/');
-    date[0] = date[0].padStart(2, '0');
-    date[1] = date[1].padStart(2, '0');
-    date = [date[1], date[0], date[2]];
-    date = date.join('');
-    return date;
-  };
-  const fileDateSufix = dateFormater(date);
+ const date = new Date().toLocaleDateString();
+ const dateFormater = (date) => {
+   date = date.split('/');
+   date[0] = date[0].padStart(2, '0');
+   date[1] = date[1].padStart(2, '0');
+   date = [date[1], date[0], date[2]];
+   date = date.join('');
+   return date;
+ };
+ const fileDateSufix = dateFormater(date);
 
-  res.download('./public/table.pdf', `LotoMania_${fileDateSufix}.pdf`);
+ res.download('./public/table.pdf', `LotoMania_${fileDateSufix}.pdf`);
 });
 
-app.listen(8080, function () {
+app.listen(3000, function () {
   console.log('Server started');
 });
